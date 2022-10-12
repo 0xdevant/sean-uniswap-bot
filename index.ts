d38555968367d3eaee6cbabb4a2ae024b10591d1
knyumatt <knyuwork@gmail.com>
2022-10-12 18:41:16 +0800

    fix: map route

--- index.ts
+++ index.ts
@@ -28,7 +28,8 @@ const UNISWAP_V2ROUTER_ADDRESS = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
 // const UNISWAP_V2_FACTORY_ABI =
 //   '[{"inputs":[{"internalType":"address","name":"_feeToSetter","type":"address"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"token0","type":"address"},{"indexed":true,"internalType":"address","name":"token1","type":"address"},{"indexed":false,"internalType":"address","name":"pair","type":"address"},{"indexed":false,"internalType":"uint256","name":"","type":"uint256"}],"name":"PairCreated","type":"event"},{"constant":true,"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"allPairs","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"allPairsLength","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"tokenA","type":"address"},{"internalType":"address","name":"tokenB","type":"address"}],"name":"createPair","outputs":[{"internalType":"address","name":"pair","type":"address"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"feeTo","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"feeToSetter","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"","type":"address"},{"internalType":"address","name":"","type":"address"}],"name":"getPair","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"_feeTo","type":"address"}],"name":"setFeeTo","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"_feeToSetter","type":"address"}],"name":"setFeeToSetter","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"}]';
 
-const SEAN_ADDRESS = "0xA719CB79Af39A9C10eDA2755E0938bCE35e9DE24";
+// const SEAN_ADDRESS = "0xA719CB79Af39A9C10eDA2755E0938bCE35e9DE24";
+const SEAN_ADDRESS = "0xB8c77482e45F1F44dE1745F52C74426C631bDD52";
 const USDT_ADDRESS = "0xdAC17F958D2ee523a2206206994597C13D831ec7";
 const USDC_ADDRESS = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
 const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
@@ -89,12 +90,15 @@ const fetchSeanUSDPair = async () => {
   const slippageTolerance = new Percent("1000", "10000"); // 1000 bips, or 10%
 
   const amountOutMin = trade.minimumAmountOut(slippageTolerance).raw; // needs to be converted to e.g. hex
-  const path = [
-    SEAN_USDT_PAIR
-      ? USDT_Token[SEAN_Token.chainId].address
-      : USDC_Token[SEAN_Token.chainId].address,
-    SEAN_Token.address,
-  ];
+  console.log(trade.route.path);
+  // route
+  // const path = [
+  //   SEAN_USDT_PAIR
+  //     ? USDT_Token[SEAN_Token.chainId].address
+  //     : USDC_Token[SEAN_Token.chainId].address,
+  //   SEAN_Token.address,
+  // ];
+  const path = trade.route.path.map(({ address }) => address);
   const to = signer.address; // should be a checksummed recipient address
   const deadline = Math.floor(Date.now() / 1000) + 60 * 20; // 20 minutes from the current Unix time
   const value = trade.inputAmount.raw; // // needs to be converted to e.g. hex
@@ -146,6 +150,7 @@ async function swap(
 }
 
 (async () => {
-  task = cron.schedule("0-59/3 * * * * *", fetchSeanUSDPair);
-  task.start();
+  await fetchSeanUSDPair();
+  // task = cron.schedule("0-59/3 * * * * *", fetchSeanUSDPair);
+  // task.start();
 })();
